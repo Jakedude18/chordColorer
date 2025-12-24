0c0316b1214e99afb3e0feca401e81b7b940c719
Jake Kerrigan <jakekerrigan@macbookpro.lan>
2025-12-24 14:31:28 -0600

    tailwind styled up

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",   // Look for Tailwind classes in index.html
+    "./css/input.css" // And in your input.css file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
